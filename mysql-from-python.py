d9f12b06ae695972bf6b03567c15ce5c21833083
ifti-khan <ifti_k@hotmail.com>
2021-03-16 20:22:35 +0000

    starting python mysql tutorials

---
+++ mysql-from-python.py
@@ -0,0 +1,19 @@
+import os
+import pymysql
+
+
+username = os.getenv('ifti_gitpod')
+
+
+connection = pymysql.connect(
+    host='localhost', user=username, password='', db='Chinook')
+
+
+try:
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM Artist;"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(result)
+finally:
+    connection.close()
